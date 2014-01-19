a85f917115663d1dcad08bd4690e850a89988cc6
Hasham Malik <hasham2@gmail.com>
2014-01-19 19:36:17 +0500

    Added admin views and model changes

--- src/belog/models/posts.clj
+++ src/belog/models/posts.clj
@@ -9,6 +9,20 @@
 ;; to get current time in milliseconds
 (def now 
   (str (java.sql.Timestamp. (System/currentTimeMillis))))
+;; Fetch one blog post with given ID
+
+(defn get [id]
+  (j/query sqlite-db
+    (s/select * :posts (s/where {:id id}))))
+
+(defn create [params]
+  (j/insert! sqlite-db :posts (merge params {:created_at now})))
+
+(defn save [id params]
+  (j/update! sqlite-db :posts params (s/where {:id id})))
+
+(defn delete [id]
+  (j/delete! sqlite-db :posts (s/where {:id id})))
 
 ;; Fetch all the blog posts in database
 (defn all []
